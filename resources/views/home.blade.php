cf3d534a08a0966f7dbd509c8dc42f84498cdb6b
Asutin12 <asuto12271818@docomo.ne.jp>
2022-07-07 07:39:15 +0900

    [NODIFY]layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body{padding: 30px;}
+        .header-right{display: flex;align-items: center;justify-content: space-between;}
+        .header-list{display: flex;align-items: center;width: 600px;justify-content: space-between;}
+    </style>
+</head>
+<body>
+<header>
+    <div class="header-right">
+        <h1 class="header-title">Title</h1>
+        <h2 class="search">検索</h2>
+        <ul class="header-list">
+            <li class="header-item"><a href="">会員登録</a></li>
+            <li class="header-item"><a href="">ログイン</a></li>
+            <li class="header-item"><a href="">お知らせ</a></li>
+            <li class="header-item"><a href="">出品</a></li>
+        </ul>
+    </div>
+</header>
+<main>
+
+</main>
+<footer>
+    
+</footer>
+</body>
+</html>
