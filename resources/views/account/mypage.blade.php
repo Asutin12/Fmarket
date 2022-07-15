8a50b3d7bfa14474b8a9315ae09dc2e7d710605e
Asutin12 <asuto12271818@docomo.ne.jp>
2022-07-16 01:31:24 +0900

    [MODIFY]layout

--- resources/views/account/mypage.blade.php
+++ resources/views/account/mypage.blade.php
@@ -6,7 +6,7 @@
 <p>USER: {{$user->family_name . ' ' . $user->first_name }}</p>
 <p>Birth_day: {{$user->birth_day_yy . '年' . $user->birth_day_mm . '月' . $user->birth_day_dd . '日'}}</p>
 @else
-<p>※ログインしていません(<a href="/login">ログイン</a>|<a href="/register">登録</a>)</p>
+<p>※ログインしていません(<a href="/login">ログイン</a>|<a href="/register">会員登録</a>)</p>
 @endif
 
 @endsection
