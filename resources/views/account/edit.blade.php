dcd6219e4fe917484e55749fbe5c4f864f2a1584
Asutin12 <asuto12271818@docomo.ne.jp>
2022-09-11 22:59:29 +0900

    [MODIFY]account

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -6,9 +6,9 @@
                 {{-- <p>USER: {{$user->family_name . ' ' . $user->first_name }}</p> --}}
                 {{-- <p>Birth_day: {{$user->birth_day_yy . '年' . $user->birth_day_mm . '月' . $user->birth_day_dd . '日'}}</p> --}}
 
-            @else
+            {{-- @else
             <p>※ログインしていません(<a href="/login">ログイン</a>|<a href="/register">会員登録</a>)</p>
-            @endif --}}
+            @endif  --}}
 
             <h3>編集</h3>
 @endsection
