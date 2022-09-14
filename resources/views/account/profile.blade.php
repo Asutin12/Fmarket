6cc3fcc9117de02286ed767bf56a61ccedb51514
Asutin12 <asuto12271818@docomo.ne.jp>
2022-09-14 21:51:50 +0900

    [MODIFY]layout

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -4,9 +4,9 @@
 <h1>profileページ</h1>
 <p>{{$user->name}}</p>
 <hr>
-{{-- <a class="item" href="{{route('items.index',['item_id'=>$item->id])}}">
+<a class="item" href="{{route('items.index',['item_id'=>$item->id])}}">
 <img src="{{ '/storage/' . $item->image}}" width="200px"  alt="">
 <p>{{$item->name}}</p>
 <p>¥{{$item->price}}</p>
-</a> --}}
+</a>
 @endsection
