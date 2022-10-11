2ecbd91865cd32cde5a326ec764ffca336f9ee2d
Asutin12 <asuto12271818@docomo.ne.jp>
2022-10-11 20:45:24 +0900

    [MODIFY]profile

--- resources/views/account/profile.blade.php
+++ resources/views/account/profile.blade.php
@@ -4,12 +4,14 @@
 <h1>profileページ</h1>
 <p>{{$user->name}}</p>
 <hr>
-@foreach ($user->item as $item)
+<div class="item-box">
+    @foreach ($user->item as $item)
     <a class="item" href="{{route('items.index',['item_id'=>$item->id])}}">
-    <img src="{{ '/storage/' . $item->image}}" width="200px"  alt="">
-    <p>{{$item->name}}</p>
-    <p>¥{{$item->price}}</p>
-    </a>
-@endforeach
+        <img src="{{ '/storage/' . $item->image}}" width="200px"  alt="">
+        <p>{{$item->name}}</p>
+        <p>¥{{$item->price}}</p>
+        </a>
+    @endforeach
+</div>
 
 @endsection
