4383fdb460146c5a2c56d8a83f1ddb30235d2180
Asutin12 <asuto12271818@docomo.ne.jp>
2022-10-17 10:46:10 +0900

    [MODIFY]edit

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -14,13 +14,27 @@
             <p>{{$user->name}}</p>
             <form method="post" action="{{route('profile.edit',$user)}}">
                 @csrf
+                <input type="hidden" name="id" value="{{$user->id}}">
                 <div class="row mb-3">
                     <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('ニックネーム') }}</label>
 
                     <div class="col-md-6">
-                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$user->name}}" required autocomplete="name" autofocus>
 
                         @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="introduction" class="col-md-4 col-form-label text-md-end">{{ __('自己紹介文') }}</label>
+
+                    <div class="col-md-6">
+                        <textarea id="introduction" type="text" class="form-control @error('introduction') is-invalid @enderror" name="introduction" value="{{$user->introduction}}" required autocomplete="introduction" autofocus></textarea>
+
+                        @error('introduction')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
