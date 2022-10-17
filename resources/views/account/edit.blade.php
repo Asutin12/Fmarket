71b15dfce0de7a59174ef16c6b7586fd49ef7a22
Asutin12 <asuto12271818@docomo.ne.jp>
2022-10-17 12:04:21 +0900

    [MODIFY]mypage

--- resources/views/account/edit.blade.php
+++ resources/views/account/edit.blade.php
@@ -11,27 +11,25 @@
             @endif  --}}
 
             <h3>プロフィール設定</h3>
-            <p>{{$user->name}}</p>
             <img src="{{ '/storage/' . $user->image}}" width="100px"  alt="">
             <form method="post" action="{{route('profile.edit',$user)}}" enctype="multipart/form-data">
                 @csrf
                 <input type="hidden" name="id" value="{{$user->id}}">
                 <div class="row mb-3">
-                    <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('ニックネーム') }}</label>
+                    <label for="image" class="col-md-4 col-form-label text-md-end">{{ __('プロフィール画像') }}</label>
 
-                    <div class="row mb-3">
-                        <label for="image" class="col-md-4 col-form-label text-md-end">{{ __('プロフィール画像') }}</label>
+                    <div class="col-md-6">
+                        <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('$image') }}" required autocomplete="image" autofocus>
 
-                        <div class="col-md-6">
-                            <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('$image') }}" required autocomplete="image" autofocus>
-
-                            @error('image')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
+                        @error('image')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
                     </div>
+                </div>
+                <div class="row mb-3">
+                    <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('ニックネーム') }}</label>
                     <div class="col-md-6">
                         <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{$user->name}}" required autocomplete="name" autofocus>
 
@@ -58,7 +56,7 @@
                 <div class="row mb-0">
                     <div class="col-md-6 offset-md-4">
                         <button type="submit" class="btn btn-primary">
-                            {{ __('更新') }}
+                            {{ __('更新する') }}
                         </button>
                     </div>
                 </div>
