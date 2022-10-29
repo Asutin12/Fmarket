28a34822977b02deb77ac86b30cc0d3935c50e3e
Asutin12 <asuto12271818@docomo.ne.jp>
2022-10-29 18:15:06 +0900

    [layout]

--- resources/views/home/mylist.blade.php
+++ resources/views/home/mylist.blade.php
@@ -3,7 +3,6 @@
 @section('detail')
     <a href="{{route('home.index')}}">おすすめ</a>
     <a href="{{route('home.mylist',['user_id'=>$user->id])}}">マイリスト</a>
-    <a href="">ピックアップ</a>
 @endsection
 
 @section('content')
