5ac58df17c0fad41ea06a44c7c8a6d00b40ef27a
Asutin12 <asuto12271818@docomo.ne.jp>
2022-11-13 20:20:48 +0900

    [MODIFY]layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    Home
+                    Title
                 </a>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
